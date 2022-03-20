2483b568847267314df97aad87b48e4a0a2cc502
KasiaHajduk <kasiahajduk2014@gmail.com>
2022-03-20 23:14:27 +0100

    Update FeedbackOptions.js

--- src/components/FeedbackOptions.js
+++ src/components/FeedbackOptions.js
@@ -1,14 +1,11 @@
 import './Feedback.css';
-
 import PropTypes from 'prop-types';
 
-const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  return <button className="feedback__button" name={options} onClick={onLeaveFeedback}>{options}</button>
+export default function FeedbackOptions ({ options, onLeaveFeedback }) {
+  return (<button className="feedback__button" name={options} onClick={onLeaveFeedback}>{options}</button>);
 }
 
 FeedbackOptions.propTypes = {
   options: PropTypes.string.isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
 }
-
-export default FeedbackOptions;
